38521ed67bd50261fce84abf78492adeac20b844
Oto Brglez <otobrglez@gmail.com>
2015-07-25 17:15:51 +0200

    Better MAC address. Possibly valid.

--- mac_changer.sh
+++ mac_changer.sh
@@ -4,11 +4,21 @@ if [ "$(id -u)" != "0" ]; then
   echo "ERROR: Run this script as root!"
 fi
 
-export NEW_MAC=`openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`
+function valid_mac() {
+	openssl rand -hex 1 | \
+		tr '[:lower:]' '[:upper:]' | \
+		xargs echo "obase=2;ibase=16;" | \
+		bc | \
+		cut -c1-6 | \
+		sed 's/$/00/' | \
+		xargs echo "obase=16;ibase=2;" | \
+		bc | \
+		sed "s/$/:$(openssl rand -hex 5 | \
+		sed 's/\(..\)/\1:/g; s/.$//' | tr '[:lower:]' '[:upper:]')/"
+}
 
-ifconfig en0 ether $NEW_MAC && \
-  ifconfig en0 down && \
-  ifconfig en0 up
+NEW_MAC=$(valid_mac)
 
+ifconfig en0 ether $NEW_MAC && ifconfig en0 down && ifconfig en0 up
 
 echo "Mac changed to $NEW_MAC"
